bfad7afb672732cc22d71ca1b7a69dfcf6e34a50
Coral <cnight18@hotmail.com>
2023-06-05 10:17:51 -0300

    trying to add sidebar for search and history

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function GetInfo(){
+    const
+}
+
+fetch("")
+// waiting for link to work //
+// https://api.openweathermap.org/data/2.5/forecast?q={Japan}&appid={e5382fffbfa3ab43b29d0877aefa5100} //
