8ebddab556fe8b9e41967c04739840780f90af94
Coral <cnight18@hotmail.com>
2023-06-05 19:58:56 -0300

    no idea how to make api work

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
-function GetInfo(){
-    const
-}
+function getInfo() {
+    const city = document.getElementById('city-input').value; //html line 40 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; 
+    const apiKey = 'e5382fffbfa3ab43b29d0877aefa5100'; 
+  
+    fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
 
-fetch("")
-// waiting for link to work //
-// https://api.openweathermap.org/data/2.5/forecast?q={Japan}&appid={e5382fffbfa3ab43b29d0877aefa5100} //
+    for 
+    for 
+    for 
+
