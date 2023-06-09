36a1333b0392e4de40fd8fce10e6a0d551f84742
Coral <cnight18@hotmail.com>
2023-06-08 22:52:08 -0300

    api not working , made new key, starting over

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,2 @@
-function getInfo() {
-    const city = document.getElementById('city-input').value; //html line 40 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; 
-    const apiKey = 'e5382fffbfa3ab43b29d0877aefa5100'; 
-  
-    fetch(apiUrl)
-.then(response => response.json())
-.then(data => {
-
-    for 
-    for 
-    for 
+const API_KEY = '53423b9a7c722184334c598bb74ab66';
 
